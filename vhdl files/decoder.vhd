7b329b6953e8b020fb4db79daec7a65c0446cc1e
philopateer nabil <41947911+phella@users.noreply.github.com>
2019-12-21 08:18:38 -0800

    fix seig_generator

--- vhdl files/decoder.vhd
+++ vhdl files/decoder.vhd
@@ -3,7 +3,7 @@ USE IEEE.std_logic_1164.all;
 USE ieee.numeric_std.ALL;
 
 ENTITY decoder IS
-  GENERIC ( N : INTEGER := 16 ) ;
+  GENERIC ( N : INTEGER := 3 ) ;
 	PORT( grp : in std_logic_vector(N-1 downto 0);
 	      signals : out std_logic_vector ( (2**N)-1 downto 0)
 	     	 );
@@ -11,9 +11,9 @@ END ENTITY decoder;
 
 
 ARCHITECTURE arch1 of decoder IS
+
 BEGIN  
   
-signals <= (others => '0'); 
-signals(to_integer(unsigned(grp))) <= '1';
+signals <= ( to_integer(unsigned(grp)) => '1' , others=>'0' );
 
 END arch1;  
